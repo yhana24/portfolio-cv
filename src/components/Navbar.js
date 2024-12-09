ec250fadffc0c2d9749ffe3bc5e3be99bef367be
jrzstflr <jrzstflr@outlook.com>
2024-12-09 18:46:02 +0800

    Add Logo in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,35 +4,42 @@ import { FaHome, FaUserAlt, FaFolder, FaEnvelope } from "react-icons/fa"; // Rea
 import { Link } from 'react-scroll'; // Import Link from react-scroll for smooth scrolling
 import '../Navbar.css'; // Ensure this path is correct
 
+// Import the logo image
+import jrzLogo from '../assets/images/jrzlogo.png'; // Adjust the path if needed
+
 const PortfolioNavbar = () => {
   return (
-    <BootstrapNavbar expand="lg" fixed="top" className="navbar">
-      <BootstrapNavbar.Brand href="#home">My Portfolio</BootstrapNavbar.Brand>
-      <BootstrapNavbar.Toggle aria-controls="navbar-nav" />
-      <BootstrapNavbar.Collapse id="navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link>
-            <Link to="home" smooth={true} duration={500}>
-              <FaHome /> Home
-            </Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="about" smooth={true} duration={500}>
-              <FaUserAlt /> Certifications
-            </Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="projects" smooth={true} duration={500}>
-              <FaFolder /> Projects
-            </Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="contact" smooth={true} duration={500}>
-              <FaEnvelope /> Contact Me
-            </Link>
-          </Nav.Link>
-        </Nav>
-      </BootstrapNavbar.Collapse>
+    <BootstrapNavbar expand="sm" bg="dark" variant="dark" fixed="top">
+      <div className="container-fluid">
+        <BootstrapNavbar.Brand href="#home">
+          <img src={jrzLogo} alt="Avatar Logo" style={{ width: "40px" }} className="rounded-pill" />
+        </BootstrapNavbar.Brand>
+        <BootstrapNavbar.Toggle aria-controls="navbar-nav" />
+        <BootstrapNavbar.Collapse id="navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link>
+              <Link to="home" smooth={true} duration={500}>
+                <FaHome /> Home
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="about" smooth={true} duration={500}>
+                <FaUserAlt /> Certifications
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="projects" smooth={true} duration={500}>
+                <FaFolder /> Projects
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="contact" smooth={true} duration={500}>
+                <FaEnvelope /> Contact Me
+              </Link>
+            </Nav.Link>
+          </Nav>
+        </BootstrapNavbar.Collapse>
+      </div>
     </BootstrapNavbar>
   );
 };
